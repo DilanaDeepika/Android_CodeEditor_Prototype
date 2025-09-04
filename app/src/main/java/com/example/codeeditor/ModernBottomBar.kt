dae4e3e7874676984c234ffec31b37dd0a70440d
Dilana Deepika <dilanasuwendra@gmail.com>
2025-09-05 05:23:04 +0530

    more optimized

--- app/src/main/java/com/example/codeeditor/ModernBottomBar.kt
+++ app/src/main/java/com/example/codeeditor/ModernBottomBar.kt
@@ -1,6 +1,6 @@
 package com.example.codeeditor
 
-import android.content.Context
+
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
